98ef62a620e92239a167b83df94669bba5943bb6
r4uzn <726ksm@gmail.com>
2026-08-04 22:52:51 +0900

    Remove the BULKHEAD post list blurb.

--- app/content.ts
+++ app/content.ts
@@ -25,8 +25,7 @@ export const posts: BlogPost[] = [
   {
     slug: "system-hacking-mitigation-bulkhead",
     title: "System Mitigation - BULKHEAD",
-    content:
-      "해킹 기법과 mitigation의 공진화를 따라가며, NDSS 2025 BULKHEAD를 통해 PKS 기반 커널 컴포넌트 격리의 방향성을 살펴봅니다.",
+    content: "",
     date: "2026-08-03",
     author: "r4uzn",
   },
